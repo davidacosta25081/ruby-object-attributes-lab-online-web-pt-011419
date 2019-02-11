7be816d2b16c159db8444a942d5f81e825b6f932
David Acosta <davidacosta25081@yahoo.com>
2019-02-11 19:38:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
 
-def name = (new_name)
+def name= (new_name)
   @name = new_name
 end
 
@@ -8,7 +8,7 @@ def name
   @name
 end
 
-def breed = (new_breed)
+def breed= (new_breed)
   @breed = new_breed
 end
 
